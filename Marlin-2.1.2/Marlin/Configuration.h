67ed1eeccd1efa9dca122e0452cd8f78a6ac0cd9
kikukato <jpjphenriqueze@gmail.com>
2024-08-28 17:23:54 -0500

    marlin con 3 servos y codigo arduino para modular potencia de laser

--- Marlin-2.1.2/Marlin/Configuration.h
+++ Marlin-2.1.2/Marlin/Configuration.h
@@ -3345,12 +3345,12 @@
  * Set this manually if there are extra servos needing manual control.
  * Set to 0 to turn off servo support.
  */
-#define NUM_SERVOS 2 // Note: Servo index starts with 0 for M280-M282 commands
+#define NUM_SERVOS 3 // Note: Servo index starts with 0 for M280-M282 commands
 
 // (ms) Delay before the next move will start, to give the servo time to reach its target angle.
 // 300ms is a good value but you can try less delay.
 // If the servo can't reach the requested position, increase it.
-#define SERVO_DELAY { 300, 300 }
+#define SERVO_DELAY { 300, 300, 300 }
 
 // Only power servos during movement, otherwise leave off to prevent jitter
 #define DEACTIVATE_SERVOS_AFTER_MOVE
